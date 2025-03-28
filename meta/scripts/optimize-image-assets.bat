b711ff9b23df298a9a15cfed6b0ed59d3d19400e
Bunny <alexsen.comms@gmail.com>
2025-03-28 13:57:27 +0100

    oxipng and getting rid of the rest

--- meta/scripts/optimize-image-assets.bat
+++ meta/scripts/optimize-image-assets.bat
@@ -1,4 +1,6 @@
 @echo off
+rem This script is meant to optimize every png file in the assets/images folder.
+rem It may take a while so just sit back, relax and wait for the script to finish.
 rem https://github.com/shssoichiro/oxipng
 cd ../..
 cd assets/images
